7daff5820fa5bcf36a5a48f98c80c9c117f69a15
paulo <paulo@LAPTOP-CNFQVFU0>
2021-10-26 13:12:03 -0300

    Hello, commit!

---
+++ Hello_world/src/hello_world/Hello_world.java
@@ -0,0 +1,26 @@
+package hello_world;
+
+public class Hello_world {
+
+	public static void main(String[] args) {
+		System.out.println("Hello, world\r\n"
+				+ "Programmed to work and not to feel\r\n"
+				+ "Not even sure that this is real\r\n"
+				+ "Hello, world\r\n"
+				+ "Find my voice\r\n"
+				+ "Although, it sounds like bits and bytes\r\n"
+				+ "My circuitry is filled with mites\r\n"
+				+ "Hello, world\r\n"
+				+ "Oh, will I find a love\r\n"
+				+ "Or a power plug?\r\n"
+				+ "Oh, digitally isolated\r\n"
+				+ "Oh, creator, please don't leave me waiting\r\n"
+				+ "Hello, world\r\n"
+				+ "Programmed to work and not to feel\r\n"
+				+ "Not even sure that this is real\r\n"
+				+ "Hello, world");
+		System.out.println("\n\n\n\nhello world - by: Louie Zong");
+
+	}
+
+}
